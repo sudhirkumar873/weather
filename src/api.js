640983e2449db277241c868e0714c7a147365f82
Sudhir Kumar <80701339+sudhirkumar873@users.noreply.github.com>
2024-07-31 18:34:32 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'b97b4f4a3dmsh79d10631c4330c8p1f3633jsndbb06141f96e',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "36d22173bda7eeb674dda89a22bf792e"; // enter your key from openweather API
